a928a2c690d7ae2ac7173543e7f2bfa73aeb3900
Julia Deconto <decontojulia31@gmail.com>
2026-05-27 19:28:07 -0300

    feat: Implement CategoryDAO CRUD
    - Added create, read (by id, by name, all) and update methods
    - Uses PreparedStatement to prevent SQL injection
    - Maps ResultSet to Category model objects
    - Handles SQL exceptions inside DAO layer
    
    Also updated Category model:
    - changed id type from Long to int to match database schema

---
+++ src/main/java/br/com/librarysystem/dao/CategoryDAO.java
@@ -0,0 +1,148 @@
+package br.com.librarysystem.dao;
+
+import br.com.librarysystem.model.Category;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryDAO {
+
+    private Connection connection;
+
+    public CategoryDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void create(Category category) {
+
+        String sql = "INSERT INTO category(name) VALUES (?)";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, category.getName());
+
+            statement.executeUpdate();
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Category findById(Long id) {
+
+        String sql = "SELECT * FROM category WHERE id = ?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setLong(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Category category = new Category();
+
+                category.setId(resultSet.getInt("id"));
+                category.setName(resultSet.getString("name"));
+
+                return category;
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public Category findByName(String name) {
+
+        String sql = "SELECT * OF category WHERE name = ?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, name);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                Category category = new Category();
+
+                category.setId(resultSet.getInt("id"));
+                category.setName(resultSet.getString("name"));
+
+                return category;
+            }
+
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public List <Category> findAll() {
+
+        String sql = "SELECT * FROM category";
+
+        List<Category> categories = new ArrayList<>();
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+
+                Category category = new Category();
+
+                category.setId(resultSet.getInt("id"));
+                category.setName(resultSet.getString("name"));
+
+                categories.add(category);
+            }
+
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return categories;
+    }
+
+    public void update(Category category) {
+
+        String sql = "UPDATE category SET name ? WHERE id = ?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, category.getName());
+            statement.setInt(2, category.getId());
+
+            statement.executeUpdate();
+
+            statement.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/br/com/librarysystem/model/Category.java
+++ src/main/java/br/com/librarysystem/model/Category.java
@@ -2,23 +2,27 @@ package br.com.librarysystem.model;
 
 public class Category {
 
-    private Long id;
+    private int id;
     private String name;
 
     public Category() {
     }
 
-    public Category(Long id, String name) {
+    public Category(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setName(String name) {
